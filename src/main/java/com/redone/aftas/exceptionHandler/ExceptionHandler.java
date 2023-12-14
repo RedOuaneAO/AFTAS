7bf7af8dce9067f994c05f9687aa59c0c8bd81b0
RedOuaneAO <redouaneao97@gmail.com>
2023-12-14 01:52:57 +0100

    add global exception handler

--- src/main/java/com/redone/aftas/exceptionHandler/ExceptionHandler.java
+++ src/main/java/com/redone/aftas/exceptionHandler/ExceptionHandler.java
@@ -1,7 +1,30 @@
 package com.redone.aftas.exceptionHandler;
 
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@RestControllerAdvice
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+
+@ControllerAdvice
 public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Map<String,Object>> handleRuntimeException(RuntimeException ex) {
+        Map<String,Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        response.put("status","error");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+
 }
