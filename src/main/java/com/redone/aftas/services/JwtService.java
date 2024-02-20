60751b9177561bfdefd18c6c0d91b60a5bc8d6aa
RedOuaneAO <redouaneao97@gmail.com>
2024-02-20 14:53:52 +0100

    create jwt service

---
+++ src/main/java/com/redone/aftas/services/JwtService.java
@@ -0,0 +1,29 @@
+package com.redone.aftas.services;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+public interface JwtService {
+    String extractUsername(String jwtToken);
+
+    Claims extractAllClaims(String token);
+
+    Key getSignInKey();
+
+    String generateToken(UserDetails userDetails);
+
+    String generateToken(Map<String, Object> extraClaims, UserDetails userDetails);
+
+    <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
+
+    boolean isTokenExpired(String token);
+
+    Date extractExpiration(String token);
+}
